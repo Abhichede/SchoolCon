a9fb27b42a2a241ec151b0e8050aae9d50b8708b
Abhijit <abhichede777@gmail.com>
2018-01-30 20:25:23 +0530

    online update type of att_data in attendance
    might need to change for later errors

--- db/migrate/20180110171518_change_column_type_of_attendance.rb
+++ db/migrate/20180110171518_change_column_type_of_attendance.rb
@@ -1,5 +1,5 @@
 class ChangeColumnTypeOfAttendance < ActiveRecord::Migration[5.1]
   def change
-    change_column :attendances, :att_data, :json
+    change_column :attendances, :att_data, 'json USING CAST(att_data AS json)'
   end
 end
