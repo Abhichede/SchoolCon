766b6532bd9ea211ba6e9f14b608ff0c09ce8420
abhichede777 <abhichede777@gmail.com>
2018-03-06 19:05:20 +0530

    path changed for leaving certificate

--- app/uploaders/leaving_certificate_uploader.rb
+++ app/uploaders/leaving_certificate_uploader.rb
@@ -10,7 +10,7 @@ class LeavingCertificateUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/leaving_certs/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -42,6 +42,6 @@ class LeavingCertificateUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    "#{model.class.to_s.underscore}#{model.id}_leving_certificate.jpg" if original_filename
+    "#{model.class.to_s.underscore}_#{model.id}_leving_certificate.jpg" if original_filename
   end
 end
