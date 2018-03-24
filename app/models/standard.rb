10733d4959dd1b3e4fa8b48475949cae8c749f24
Abhijit <abhichede777@gmail.com>
2018-03-24 22:23:20 +0530

    Standard wise fee collection

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -6,4 +6,58 @@ class Standard < ApplicationRecord
 
   has_many :attendances
   has_many :fee_categories
+
+  def total_allocated_fee
+    fee = 0
+    students.each do |student|
+      fee += student.current_total_fee
+    end
+
+    fee
+  end
+
+  def total_paid_fee
+    fee = 0
+    students.each do |student|
+      fee += student.current_total_paid
+    end
+
+    fee
+  end
+
+  def total_discount
+    fee = 0
+    students.each do |student|
+      fee += student.current_total_discount
+    end
+
+    fee
+  end
+
+  def total_fine
+    fee = 0
+    students.each do |student|
+      fee += student.current_total_fine
+    end
+
+    fee
+  end
+
+  def total_instant_fee
+    fee = 0
+    students.each do |student|
+      fee += student.current_total_instant_fee
+    end
+
+    fee
+  end
+
+  def total_balance_amount
+    fee = 0
+    students.each do |student|
+      fee += student.current_total_paid
+    end
+
+    fee
+  end
 end
