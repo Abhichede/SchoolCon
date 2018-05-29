050df2ed6b6ff3250c07164b1f9220ecb83755c4
Abhichede777 <Abhichede777@gmail.com>
2018-05-29 13:06:49 +0530

    Changes in links of sidebars, redesigned parents module. and added function to change student_id of parents account in Users module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,6 +53,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:password, :password_confirmation, :student_id, roles: []])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :student_id, roles: []])
   end
 end
