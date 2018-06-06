dc37b3271af46ddb9e2594511340dd02f4189d7a
Abhichede777 <Abhichede777@gmail.com>
2018-06-06 13:31:34 +0530

    SMS Unmuted.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def current_institute
     SchoolInfo.first
   end
+
+  def random_boot_panel
+    panels = %w(default danger success info warning)
+
+    panels.sample
+  end
 end
