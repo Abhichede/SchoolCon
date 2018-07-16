8ec57e2db301ef58335fba34f63d3fcf32975ba7
Abhichede777 <Abhichede777@gmail.com>
2018-07-16 15:33:11 +0530

    subject standard wise

--- db/migrate/20180716092452_change_column_type_from_attendances.rb
+++ db/migrate/20180716092452_change_column_type_from_attendances.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypeFromAttendances < ActiveRecord::Migration[5.1]
   def up
-    change_column :attendances, :date, :date
+    change_column :attendances, :date, "date USING date::date"
   end
 
   def down
