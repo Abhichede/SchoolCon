beb58a0d4e7a11448249a3ad3dfcc73d3febbeb1
abhijit <abhichede777@gmail.com>
2018-09-25 11:44:29 +0530

    Changes made in api call and leave application, homework

--- app/controllers/api/parents_controller.rb
+++ app/controllers/api/parents_controller.rb
@@ -7,30 +7,57 @@ module Api
     end
 
     def register_device_id
-      puts "Parent id: #{device_reg_params[:parent_id]}"
-      if device_reg_params[:parent_id]
-        @parent = Parent.find(device_reg_params[:parent_id])
-        @user = User.where(username: @parent.mobile).last
+      if device_reg_params[:user_type] == 'parent'
+        if device_reg_params[:parent_id]
+          @parent = Parent.find(device_reg_params[:parent_id])
+          @user = User.where(username: @parent.mobile).last
 
-        if @user
-          if @user.device_id == nil || @user.device_id != device_reg_params[:device_id]
-            @user.update(device_id: device_reg_params[:device_id])
-            respond_to do |f|
-              f.json {render json: {success: 'Device id updated.'}}
+          if @user
+            if @user.device_id == nil || @user.device_id != device_reg_params[:device_id]
+              @user.update(device_id: device_reg_params[:device_id])
+              respond_to do |f|
+                f.json {render json: {success: 'Device id updated.'}}
+              end
+            else
+              respond_to do |f|
+                f.json {render json: {error: 'Device id was updated already.'} }
+              end
             end
           else
             respond_to do |f|
-              f.json {render json: {error: 'Device id was updated already.'} }
+              f.json {render json: {error: 'User not found.'}}
             end
           end
         else
           respond_to do |f|
-            f.json {render json: {error: 'User not found.'}}
+            f.json {render json: {error: 'Related parent not found.'}}
           end
         end
       else
-        respond_to do |f|
-          f.json {render json: {error: 'Related parent not found.'}}
+        if device_reg_params[:teacher_id]
+          @teacher = Teacher.find(device_reg_params[:teacher_id])
+          @user = User.where(username: @teacher.mobile).last
+
+          if @user
+            if @user.device_id == nil || @user.device_id != device_reg_params[:device_id]
+              @user.update(device_id: device_reg_params[:device_id])
+              respond_to do |f|
+                f.json {render json: {success: 'Device id updated.'}}
+              end
+            else
+              respond_to do |f|
+                f.json {render json: {error: 'Device id was updated already.'} }
+              end
+            end
+          else
+            respond_to do |f|
+              f.json {render json: {error: 'User not found.'}}
+            end
+          end
+        else
+          respond_to do |f|
+            f.json {render json: {error: 'Related parent not found.'}}
+          end
         end
       end
     end
