84acb1e95193f55e7175ff2b5b0ac33e558a7857
abhijit <abhichede777@gmail.com>
2018-10-03 13:02:15 +0530

    Changes made in api call and leave application, homework

--- app/controllers/api/homeworks_controller.rb
+++ app/controllers/api/homeworks_controller.rb
@@ -4,6 +4,9 @@ module Api
       if params[:student_id] && params[:date]
         @division = Student.find(params[:student_id]).divisions.last
         @homeworks = @division.homeworks.where(date: params[:date])
+      elsif params[:standard_id] && params[:division_id]
+        @division = Division.find(params[:division_id])
+        @homeworks = @division.homeworks.order("date DESC")
       else
         @homeworks = Homework.all
       end
@@ -27,7 +30,7 @@ module Api
           format.json { render json: {success: 'Homework created successfully.'} }
         else
           # format.html { render :new }
-          format.json { render json: {error: 'Error occurred while saving.'} }
+          format.json { render json: {error: 'Homework not created successfully.'} }
         end
       end
     end
