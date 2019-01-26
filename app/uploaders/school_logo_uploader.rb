b8ef61613b8fc2f886b2992066898dd55e1143f0
abhijit <abhichede777@gmail.com>
2019-01-26 14:36:10 +0530

    new reports section added.

--- app/uploaders/school_logo_uploader.rb
+++ app/uploaders/school_logo_uploader.rb
@@ -28,7 +28,7 @@ class SchoolLogoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :ftp
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
